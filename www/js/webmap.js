9c085823471b39c58c6b27008072477a6a7adbfc
Macarena Parrizas <macarena.parrizas@gmail.com>
2017-08-01 08:45:08 +0200

    web map side bar
    side bar functionallity done, now working in the style

--- www/js/webmap.js
+++ www/js/webmap.js
@@ -11,3 +11,16 @@
 	var credits = L.control.attribution().addTo(map);
 	credits.addAttribution('© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>');
 	
+/************************* SIDE NAV *****************************/
+    /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+    function openNav() {
+        document.getElementById("sidenav").style.width = "250px";
+        //document.getElementById("main").style.marginLeft = "250px";
+        document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    }
+
+    function closeNav() {
+        document.getElementById("sidenav").style.width = "0";
+        //document.getElementById("main").style.marginLeft= "0";
+        document.body.style.backgroundColor = "white";
+    }
